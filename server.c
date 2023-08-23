a077e53dcc4238c020ce9d643d83647cc6336106
BojackFishman <hj528455@163.com>
2023-08-23 15:48:00 +0800

    select test

--- server.c
+++ server.c
@@ -7,55 +7,8 @@
 #include<stdlib.h>
 #include<errno.h>
 #include<pthread.h>
+#include<sys/select.h>
 
-
-struct sockInfo
-{
-    int sockfd;
-    struct sockaddr_in addr;
-    pthread_t tid;
-};
-
-// 存储传入线程的参数数据。如果定义为局部变量一层循环结束就会销毁。并且要支持可能的并发访问。
-// 线程的栈空间资源不共享，管理堆空间或许又比较复杂；教程说的，真的复杂吗？
-struct sockInfo sockinfos[128];
-
-// 子线程与客户端通信
-void* working(void* arg){
-    struct sockInfo* pinfo = (struct sockInfo*)arg;
-
-    // 输出客户端信息
-    char clienIP[16];
-    inet_ntop(AF_INET, &pinfo->addr.sin_addr.s_addr, clienIP, sizeof(clienIP));
-    unsigned short clientPort = ntohs(pinfo->addr.sin_port);
-    printf("client ip : %s, port : %d\n", clienIP, clientPort);
-
-    char recvBuf[1024] = {0};
-    char* data = "this is server\n";
-
-    // 获取客户端数据
-    while(1){
-        // printf("%d\n", pinfo->sockfd);
-        int len = read(pinfo->sockfd, &recvBuf, sizeof(recvBuf));
-        if(len == -1){
-            perror("read");
-            exit(-1);
-        }
-        else if(len > 0){
-            printf("recv client data: %s \n", recvBuf);
-        }
-        else if(len == 0){
-            printf("client closed...\n");
-            break;
-        }
-
-        write(pinfo->sockfd, recvBuf, strlen(recvBuf)+1);
-    }
-    close(pinfo->sockfd);
-
-    // 不同。关闭传入数据槽的占用
-    pinfo->sockfd = -1;
-}
 
 int main(){
 
@@ -81,60 +34,60 @@ int main(){
         exit(-1);
     }
 
-    // 监听listen
-    ret = listen(httpd, 5);
-    if(ret == -1){
-        perror("listen");
-        exit(-1);
-    }
+    // 创建一个fd_set的集合，存放要检测的文件描述符
+    fd_set rdset, tmp;
+    FD_ZERO(&rdset);
+    FD_SET(httpd, rdset);
+    int maxfd = httpd;
 
-    // 初始化线程传入参数
-    int arr_len = sizeof(sockinfos)/sizeof(sockinfos[0]);
-    memset(&sockinfos, 0, sizeof(sockinfos));
-    for(int i = 0; i < arr_len; ++i){
-        sockinfos[i].sockfd = -1;
-        sockinfos[i].tid = -1;
-    }
 
-    // 接收客户端连接
-    // 循环等待客户端连接
     while(1){
-        struct sockaddr_in clientaddr;
-        socklen_t client_addr_len = sizeof(clientaddr);
-        int clientfd;
-        clientfd = accept(httpd, (struct sockaddr *) & clientaddr, &client_addr_len);
-        if(clientfd == -1){
-            if(errno == EINTR){
-                continue;
-            }
-            perror("accept");
+
+        tmp = rdset;
+        // 调用select()进行监听
+        ret = select(maxfd+1, &tmp, NULL, NULL, NULL);
+        if(ret == -1){
+            perror("select");
             exit(-1);
-        }
+        }else if(ret == 0){
+            continue;
+        }else if(ret > 0){
+            // 检测到了有文件描述符对应缓冲区数据发生了改变
+            if(FD_ISSET(httpd, &tmp)){
+                // httpd有数据，有新的客户端连接进来
+                struct sockaddr_in clientaddr;
+                socklen_t client_addr_len = sizeof(clientaddr);
+                int clientfd;
+                clientfd = accept(httpd, (struct sockaddr *) & clientaddr, &client_addr_len);
+                FD_SET(clientfd, &rdset);
 
-        // 每一个连接进来，创建一个子线程跟客户端通信
-        struct sockInfo *pinfo;
-        for(int i = 0; i < arr_len; ++i){
-            // 分配一个可用的槽
-            if(sockinfos[i].sockfd == -1){
-                pinfo = &sockinfos[i];
-                break;
+                maxfd = maxfd > clientfd ? maxfd : clientfd;
             }
-            if(i == arr_len-1){
-                sleep(1);
-                i = -1;
+            // 轮询
+            for(int i = httpd + 1; i < maxfd; ++i){
+                if(FD_ISSET(i, &tmp)){
+                    // fd=i的客户端发来了数据
+                    char recvBuf[1024] = {0};
+                    int len = read(i, &recvBuf, sizeof(recvBuf));
+                    if(len == -1){
+                        perror("read");
+                        exit(-1);
+                    }
+                    else if(len > 0){
+                        printf("recv client data: %s \n", recvBuf);
+                        write(i, recvBuf, strlen(recvBuf)+1);
+                    }
+                    else if(len == 0){
+                        printf("client closed...\n");
+                        close(i);
+                        FD_CLR(i, &rdset);
+                    }
+
+
+                }
             }
         }
-        pinfo->sockfd = clientfd;
-        pinfo->addr = clientaddr;
-        // memcmp(&pinfo->addr, &clientaddr, client_addr_len);
-
-        pthread_create(&pinfo->tid, NULL, working, pinfo);
-
-        pthread_detach(pinfo->tid);
-
-        // close(clientfd);
     }
-
     // 关闭文件描述符
     close(httpd);
 
