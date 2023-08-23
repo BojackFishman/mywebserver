22964f27ce67cd9fc77b024c90ab389ec709dc96
BojackFishman <hj528455@163.com>
2023-08-23 18:34:08 +0800

    poll to test. On serface

--- server.c
+++ server.c
@@ -8,6 +8,7 @@
 #include<errno.h>
 #include<pthread.h>
 #include<sys/select.h>
+#include<poll.h>
 
 
 int main(){
@@ -37,53 +38,78 @@ int main(){
     // listen
     listen(httpd, 5);
 
-    // 创建一个fd_set的集合，存放要检测的文件描述符
-    fd_set rdset, tmp;
-    FD_ZERO(&rdset);
-    FD_SET(httpd, &rdset);
-    int maxfd = httpd;
-
+    // 初始化检测的文件描述符数组
+    struct pollfd fds[1024];
+    for(int i = 0; i < 1024; ++i){
+        fds[i].fd = -1;
+        fds[i].events = POLLIN;
+    }
+    fds[0].fd = httpd;
+    int nfds = 0;
 
     while(1){
 
-        tmp = rdset;
-        // 调用select()进行监听
-        ret = select(maxfd+1, &tmp, NULL, NULL, NULL);
+        // 调用poll()检测文件描述符数据
+        ret = poll(fds, nfds + 1, -1);
         if(ret == -1){
-            perror("select");
+            perror("poll");
             exit(-1);
         }else if(ret == 0){
             continue;
         }else if(ret > 0){
             // 检测到了有文件描述符对应缓冲区数据发生了改变
-            if(FD_ISSET(httpd, &tmp)){
+            if(fd[0].revents & POOLIN){
                 // httpd有数据，有新的客户端连接进来
                 struct sockaddr_in clientaddr;
                 socklen_t client_addr_len = sizeof(clientaddr);
                 int clientfd;
-                clientfd = accept(httpd, (struct sockaddr *) & clientaddr, &client_addr_len);
-                FD_SET(clientfd, &rdset);
 
-                maxfd = maxfd > clientfd ? maxfd : clientfd;
+                // 将新的文件描述符加入集合中
+                // 相比select的位操作，poll还需O(n)检测找一个空的fds槽位
+                // 注意这里的i变成了fds数组的索引，而不再指代文件描述符
+                for(int i = 1; i < 1024; ++i){
+                    if(fds[i].fd == -1){
+                        // 先检索到有fd的空槽然后再接收连接。否则可能出现接收了连接无法放入fds中的情况
+                        // 如果没有空槽了那么就等下一次poll再进行连接。但是没有取走httpd缓冲区的数据下一次还会再提醒吗
+                        clientfd = accept(httpd, (struct sockaddr *) & clientaddr, &client_addr_len);
+                        fds[i].fd = clientfd;
+                        fds[i].events = POLLIN;
+                        nfds = nfds > i ? nfds : i;
+                        break;
+                    }
+                }
+
             }
             // 轮询
-            for(int i = httpd + 1; i <= maxfd; ++i){
-                if(FD_ISSET(i, &tmp)){
+            for(int i = 1; i <= nfds; ++i){
+                // if(FD_ISSET(i, &tmp)){
+                if(fd[i].revents & POOLIN){
                     // fd=i的客户端发来了数据
                     char recvBuf[1024] = {0};
-                    int len = read(i, &recvBuf, sizeof(recvBuf));
+                    int len = read(fd[i].fd, &recvBuf, sizeof(recvBuf));
                     if(len == -1){
                         perror("read");
                         exit(-1);
                     }
                     else if(len > 0){
                         printf("recv client data: %s \n", recvBuf);
-                        write(i, recvBuf, strlen(recvBuf)+1);
+                        write(fd[i].fd, recvBuf, strlen(recvBuf)+1);
                     }
                     else if(len == 0){
                         printf("client closed...\n");
-                        close(i);
-                        FD_CLR(i, &rdset);
+                        close(fd[i].fd);
+                        // FD_CLR(i, &rdset);
+                        // 减小nfds的长度
+                        if(i == nfds){
+                            for(int j = i; j > 0; --j){
+                                if(fds[j].fd != -1){
+                                    nfds = j;
+                                    break;
+                                }
+                            }
+                        }
+                        fd[i].fd = -1;
+                        // 是否需要清空fd[i].revent ?
                     }
 
 
